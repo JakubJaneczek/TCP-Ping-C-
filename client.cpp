88dcbc75d738148838177d265b03226b62f839bc
Jakub Janeczek <jakub.janeczek.12@gmail.com>
2024-10-22 17:15:21 +0200

    Initial commit

---
+++ client.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <chrono>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+using namespace std::chrono;
+
+void runTCPClient(const std::string& address, int port) {
+    int sockfd;
+    struct sockaddr_in serv_addr;
+    char buffer[256];
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        std::cerr << "ERROR opening socket\n";
+        exit(1);
+    }
+
+    bzero((char*)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+    if (inet_pton(AF_INET, address.c_str(), &serv_addr.sin_addr) <= 0) {
+        std::cerr << "Invalid address/ Address not supported\n";
+        exit(1);
+    }
+
+    if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        std::cerr << "Connection Failed\n";
+        close(sockfd);
+        exit(1);
+    }
+
+    for (int i = 0; i < 100; ++i) {
+        auto start = high_resolution_clock::now();
+
+        std::string message = "Ping " + std::to_string(i + 1);
+        int n = write(sockfd, message.c_str(), message.length());
+        if (n < 0) {
+            std::cerr << "ERROR writing to socket\n";
+        }
+
+        bzero(buffer, 256);
+        n = read(sockfd, buffer, 255);
+        if (n < 0) {
+            std::cerr << "ERROR reading from socket\n";
+        }
+
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(stop - start);
+        std::cout << "Ping " << i + 1 << " RTT: " << duration.count() << " microseconds\n";
+    }
+
+    close(sockfd);
+}
+
+void runUDPClient(const std::string& address, int port) {
+    int sockfd;
+    struct sockaddr_in serv_addr;
+    char buffer[256];
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        std::cerr << "ERROR opening socket\n";
+        exit(1);
+    }
+
+    bzero((char*)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+    if (inet_pton(AF_INET, address.c_str(), &serv_addr.sin_addr) <= 0) {
+        std::cerr << "Invalid address/ Address not supported\n";
+        exit(1);
+    }
+
+    socklen_t serv_len = sizeof(serv_addr);
+    for (int i = 0; i < 100; ++i) {
+        auto start = high_resolution_clock::now();
+
+        std::string message = "Ping " + std::to_string(i + 1);
+        int n = sendto(sockfd, message.c_str(), message.length(), 0, (struct sockaddr*)&serv_addr, serv_len);
+        if (n < 0) {
+            std::cerr << "ERROR in sendto\n";
+        }
+
+        bzero(buffer, 256);
+        n = recvfrom(sockfd, buffer, 255, 0, (struct sockaddr*)&serv_addr, &serv_len);
+        if (n < 0) {
+            std::cerr << "ERROR in recvfrom\n";
+        }
+
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(stop - start);
+        std::cout << "Ping " << i + 1 << " RTT: " << duration.count() << " microseconds\n";
+    }
+
+    close(sockfd);
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <tcp|udp> <server_ip> <port>\n";
+        exit(1);
+    }
+
+    std::string protocol = argv[1];
+    std::string server_ip = argv[2];
+    int port = std::stoi(argv[3]);
+
+    if (protocol == "tcp") {
+        runTCPClient(server_ip, port);
+    }
+    else if (protocol == "udp") {
+        runUDPClient(server_ip, port);
+    }
+    else {
+        std::cerr << "Invalid protocol. Use 'tcp' or 'udp'.\n";
+        exit(1);
+    }
+
+    return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+void runTCPServer(int port) {
+    int sockfd, newsockfd;
+    socklen_t clilen;
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        std::cerr << "ERROR opening socket\n";
+        exit(1);
+    }
+
+    bzero((char*)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(port);
+
+    if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        std::cerr << "ERROR on binding\n";
+        close(sockfd);
+        exit(1);
+    }
+
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+
+    newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &clilen);
+    if (newsockfd < 0) {
+        std::cerr << "ERROR on accept\n";
+        close(sockfd);
+        exit(1);
+    }
+
+    for (int i = 0; i < 100; ++i) { // Handle 100 pings
+        bzero(buffer, 256);
+        n = read(newsockfd, buffer, 255);
+        if (n < 0) {
+            std::cerr << "ERROR reading from socket\n";
+        }
+        std::cout << "Received ping: " << buffer << "\n";
+
+        // Send response
+        n = write(newsockfd, "Pong", 4);
+        if (n < 0) {
+            std::cerr << "ERROR writing to socket\n";
+        }
+    }
+
+    close(newsockfd);
+    close(sockfd);
+}
+
+void runUDPServer(int port) {
+    int sockfd;
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    socklen_t clilen;
+    int n;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        std::cerr << "ERROR opening socket\n";
+        exit(1);
+    }
+
+    bzero((char*)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(port);
+
+    if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        std::cerr << "ERROR on binding\n";
+        close(sockfd);
+        exit(1);
+    }
+
+    clilen = sizeof(cli_addr);
+
+    for (int i = 0; i < 100; ++i) { // Handle 100 pings
+        bzero(buffer, 256);
+        n = recvfrom(sockfd, buffer, 255, 0, (struct sockaddr*)&cli_addr, &clilen);
+        if (n < 0) {
+            std::cerr << "ERROR in recvfrom\n";
+        }
+        std::cout << "Received ping: " << buffer << "\n";
+
+        n = sendto(sockfd, "Pong", 4, 0, (struct sockaddr*)&cli_addr, clilen);
+        if (n < 0) {
+            std::cerr << "ERROR in sendto\n";
+        }
+    }
+
+    close(sockfd);
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <tcp|udp> <port>\n";
+        exit(1);
+    }
+
+    std::string protocol = argv[1];
+    int port = std::stoi(argv[2]);
+
+    if (protocol == "tcp") {
+        runTCPServer(port);
+    }
+    else if (protocol == "udp") {
+        runUDPServer(port);
+    }
+    else {
+        std::cerr << "Invalid protocol. Use 'tcp' or 'udp'.\n";
+        exit(1);
+    }
+
+    return 0;
+}
